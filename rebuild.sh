492e3db951001f731db53a353f3ae3e16694db7a
tromshusky <tromshusky@gmail.com>
2025-07-27 21:20:34 +0000

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
+1>/dev/null 2>/dev/null nixos-generate-config --show-hardware-config --no-filesystems || { printf "\nroot rights are required! use: \n\n    run0 \n"; exit; }
+git --version || { printf "\ngit is required! use: \n\n    nix-shell -p git \n"; exit; }
 
 
-TMPDIR=$(mktemp -d)
-DEV_BTRFS=/dev/disk/by-uuid/$(findmnt -n -o UUID /nix/)
-DEV_EFI=/dev/disk/by-uuid/$(findmnt -n -o UUID /boot/efi)
+TMPDIR=$(mktemp -d) || { echo error using mktemp -d; exit; }
+DEV_BTRFS=/dev/disk/by-uuid/$(findmnt -n -o UUID /nix/) || { echo error using findmnt; exit; }
+DEV_EFI=/dev/disk/by-uuid/$(findmnt -n -o UUID /boot/efi) || { echo error using findmnt; exit; }
 
-cd $TMPDIR
-
-git clone https://github.com/tromshusky/huskyos
-cd huskyos
-echo $DEV_BTRFS > BTR
-echo $DEV_EFI > EFI
+cd $TMPDIR || exit
+git clone https://github.com/tromshusky/huskyos || exit
+cd huskyos || exit
+echo $DEV_BTRFS > BTR || exit
+echo $DEV_EFI > EFI || exit
+nixos-generate-config --show-hardware-config --no-filesystems > hardware-configuration-no-filesystems.nix || exit
+touch RPW && chmod 0600 RPW || exit
+grep ^root /etc/shadow | cut -f2 -d: > RPW
