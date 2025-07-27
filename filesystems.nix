07745b145aa15a1e7f38d33fdbac86338d92474e
tromshusky <tromshusky@gmail.com>
2025-07-27 14:21:20 +0000

    Create filesystems.nix

---
+++ filesystems.nix
@@ -0,0 +1,61 @@
+{ config, lib, pkgs, modulesPath, ... }:
+let
+  BTR_DEV = lib.fileContents ./BTR;
+  EFI_DEV = lib.fileContents ./EFI;
+in
+{
+  fileSystems."/" =
+    { device = "tmpfs";
+      fsType = "tmpfs";
+      options = [ "size=16G" ];
+    };
+
+  fileSystems."/nix" =
+    { device = BTR_DEV;
+      fsType = "btrfs";
+      options = [ "subvol=@huskyos/@nix" ];
+    };
+
+  fileSystems."/boot" =
+    { device = BTR_DEV;
+      fsType = "btrfs";
+      options = [ "subvol=@huskyos/@boot" ];
+    };
+
+  fileSystems."/userdata" =
+    { device = BTR_DEV;
+      fsType = "btrfs";
+      options = [ "subvol=@huskyos/@userdata" ];
+    };
+
+  fileSystems."/root" =
+    { device = BTR_DEV;
+      fsType = "btrfs";
+      options = [ "subvol=@huskyos/@userdata/@root" ];
+    };
+
+  fileSystems."/var/lib" =
+    { device = BTR_DEV;
+      fsType = "btrfs";
+      options = [ "subvol=@huskyos/@userdata/var/@lib" ];
+    };
+
+  fileSystems."/home" =
+    { device = BTR_DEV;
+      fsType = "btrfs";
+      options = [ "subvol=@huskyos/@userdata/@home" ];
+    };
+
+  fileSystems."/etc/NetworkManager" =
+    { device = BTR_DEV;
+      fsType = "btrfs";
+      options = [ "subvol=@huskyos/@userdata/etc/@NetworkManager" ];
+    };
+
+  fileSystems."/boot/efi" =
+    { device = EFI_DEV;
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+}
