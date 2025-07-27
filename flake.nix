032db3ba3f687d9df108fcce05327add10192a9f
tromshusky <tromshusky@gmail.com>
2025-07-27 14:18:46 +0000

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  outputs =
+    { self, nixpkgs }:
+    {
+      nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./configuration.nix
+          { nix.settings.experimental-features = [ "nix-command flakes" ]; }
+        ];
+      };
+    };
+}
