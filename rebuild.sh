87817e0cd23fd2da06f6f8f8db9274da1872bab0
tromshusky <tromshusky@gmail.com>
2025-07-28 20:18:22 +0000

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -21,4 +21,6 @@ printf "copying root password... "
 touch RPW && chmod 0600 RPW || exit;
 grep ^root /etc/shadow | cut -f2 -d: > RPW || exit;
 printf "done."
+printf "generating system for next boot. After its finished, you'll be prompted to reboot. Use \n    nixos-rebuild --rollback \n to rollback ..."
 nixos-rebuild boot --flake .
+printf "done.\n"
