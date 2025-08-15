2d7f9fd0334fe50a73849ee94fd01f98bcec129e
tromshusky <tromshusky@gmail.com>
2025-08-15 18:34:09 +0200

    Update flathub.nix

--- flathub.nix
+++ flathub.nix
@@ -15,12 +15,22 @@
   systemd.services.flathub-repo.wantedBy = [ "network-online.target" ];
   systemd.services.flathub-repo.enable = true;
   systemd.services.flathub-repo.script = ''
-    iFile=/var/lib/flatpak/INITIALIZED
-    [ -e $iFile ] && exit
+    doneFile=/var/lib/flatpak/INITIALIZED
+    [ -e $doneFile ] && exit
     flatpak=${pkgs.flatpak}/bin/flatpak
-    sleep 30 && 
+    notifysend=${pkgs.libnotify}/bin/notify-send
+    runuser=/run/current-system/sw/bin/runuser
+    sleep 30 &&
+    desktop_user_id=$(loginctl show-session $(loginctl show-seat seat0 | grep ActiveSession | sed 's|ActiveSession=||') | grep User | sed 's|User=||')
+    export DISPLAY=:0
+    export XDG_RUNTIME_DIR=/run/user/$desktop_user_id
+    export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$desktop_user_id/bus
+    toast=$runuser -u $desktop_user_id $notifysend
+    notification_id=$($toast -p "Installing Flathub" "wait...");
     $flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo &&
+    $toast -r $notification_id "Downloading Apps" "wait...";
     $flatpak install -y com.brave.Browser org.kde.dolphin com.mattjakeman.ExtensionManager net.mullvad.MullvadBrowser org.mozilla.Thunderbird &&
-    touch $iFile
+    touch $doneFile;
+    $toast -r $notification_id -e "Apps installed" "...done"
   '';
 }
