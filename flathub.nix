1c91de05fbdb97ae995159a058c746926d24e1bc
tromshusky <tromshusky@gmail.com>
2025-08-15 20:39:53 +0200

    Update flathub.nix

--- flathub.nix
+++ flathub.nix
@@ -21,11 +21,14 @@
     notifysend=${pkgs.libnotify}/bin/notify-send
     runuser=/run/current-system/sw/bin/runuser
     sleep 30 &&
-    desktop_user_id=$(loginctl show-session $(loginctl show-seat seat0 | grep ActiveSession | sed 's|ActiveSession=||') | grep User | sed 's|User=||')
+    active_session=$(loginctl show-session seat0 | grep "^ActiveSession=" | sed 's|ActiveSession=||')
+    show_session_x=$(loginctl show-session active_session)
+    desktop_user_id=$(printf $show_session_x | grep "^User" | sed 's|User=||')
+    desktop_user_name=$(printf $show_session_x | grep "^Name" | sed 's|Name=||')
     export DISPLAY=:0
     export XDG_RUNTIME_DIR=/run/user/$desktop_user_id
     export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$desktop_user_id/bus
-    toast="$runuser -u $desktop_user_id $notifysend"
+    toast="$runuser -u $desktop_user_name $notifysend"
     notification_id=$($toast -p "Installing Flathub" "wait...");
     $flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo &&
     $toast -r $notification_id "Downloading Apps" "wait...";
