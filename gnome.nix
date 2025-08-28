1fd863021daa1dec05f5ac4d236c7f5afb43c196
tromshusky <tromshusky@gmail.com>
2025-08-28 13:59:11 +0200

    Update gnome.nix

--- gnome.nix
+++ gnome.nix
@@ -8,7 +8,7 @@ let
   dconf1.settings."org/gnome/shell/extensions/touchup".navigation-bar-always-show-on-monitor = ''{"name":"CMN (0x00000000)","id":"CMN::0x14b1::0x00000000"}'';
   dconf1.settings."org/gnome/mutter".experimental-features = "['scale-monitor-framebuffer']";
   dconf1.settings."org/gnome/settings-daemon/plugins/power".sleep-inactive-ac-type = "nothing";
-  dconf1.settings."org/gnome/shell".enabled-extensions = [ "dash-to-dock@micxgx.gmail.com" "touchup@mityax" ];
+  dconf1.settings."org/gnome/shell".enabled-extensions = [ "dash-to-dock@micxgx.gmail.com" "gsconnect@andyholmes.github.io" "touchup@mityax" ];
   dconf1.settings."org/gnome/shell".favorite-apps = lib.splitString " " "org.gnome.Nautilus.desktop firefox.desktop brave-browser.desktop";
 
 in
@@ -25,6 +25,7 @@ in
   environment.gnome.excludePackages = [ pkgs.gnome-tour ];
   environment.systemPackages = [
     pkgs.gnomeExtensions.dash-to-dock
+    pkgs.gnomeExtensions.gsconnect
     pkgs.gnomeExtensions.touchup
   ];
   programs.dconf.profiles.user.databases = [ dconf1 ];
