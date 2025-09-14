03e3bdfef6f0b187e93da0cd5f12dd67ba60b016
tromshusky <tromshusky@gmail.com>
2025-09-14 23:38:59 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,8 +1,21 @@
 {
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix ];
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs =
+    { self, nixpkgs }:
+    let
+      husky1 =
+        { attrs }:
+        {
+          "nixos" = nixpkgs.lib.nixosSystem {
+            modules = [
+              ./configuration.nix
+              { huskyos = attrs; }
+            ];
+          };
+        };
+    in
+    {
+      my_config = husky1;
+      nixosConfigurations = husky1 { };
     };
-  };
 }
