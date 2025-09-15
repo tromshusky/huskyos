8ae50a3fdd364faca5a3b8689f9282f7caa43ce9
tromshusky <tromshusky@gmail.com>
2025-09-15 19:09:54 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,18 +2,23 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   outputs =
     { self, nixpkgs }:
-    let
-      husky1 = attrs: {
-        "nixos" = nixpkgs.lib.nixosSystem {
-          modules = [
-            ./configuration.nix
-            { huskyos = attrs; }
-          ];
-        };
-      };
-    in
     {
-      my_config = husky1;
-      nixosConfigurations = husky1 { };
+      grub =
+        { btrfs-device, efi-device, hardware-configuration-no-filesystems }:
+        {
+          nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+            modules = [
+#              hardware-configuration-no-filesystems # lets try importing inside of configuraion.nix first
+              ./configuration.nix
+              {
+                huskyos.btrfsDevice = btrfs-device;
+                huskyos.efiDevice = efi-device;
+              }
+            ];
+          };
+        };
+      # if you want to use this flake to build the system, you have to set all the huskyos options by editing the files first
+      # trying to use this flake will lead to an error otherwise
+      nixosConfigurations = husky1 ;
     };
 }
