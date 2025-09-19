5843366225f98b6b8fc01a2aaa85f1a2684555f5
tromshusky <tromshusky@gmail.com>
2025-09-19 17:23:41 +0000

    Update huskyos.nix

--- huskyos.nix
+++ huskyos.nix
@@ -6,14 +6,12 @@ let
   EFI_OPTION.type = lib.types.str;
   EFI_OPTION.description = "";
   EFI_OPTION.example = "/dev/sda1";
-  FLAKE_OPTION.type = lib.types.path;
-  HW_OPTION.type = lib.types.submodule {};
-  HW_OPTION.default = {};
+  PATH_OPTION.type = lib.types.path;
   STEAM_OPTION.type = lib.mkEnableOption "global steam apps";
 in
 {
   options.huskyos.btrfsDevice = lib.mkOption BTR_OPTION;
   options.huskyos.efiDevice = lib.mkOption EFI_OPTION;
-  options.huskyos.flakeUri = lib.mkOption FLAKE_OPTION;
-  options.huskyos.hardware-configuration-no-filesystems = lib.mkOption HW_OPTION;
+  options.huskyos.flakeUri = lib.mkOption PATH_OPTION;
+  options.huskyos.hardwareUri = lib.mkOption PATH_OPTION;
 }
