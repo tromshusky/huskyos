a522d607e97b1212f8986ad16a739aed07270611
tromshusky <tromshusky@gmail.com>
2025-09-20 21:23:03 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   inputs.huskyos.url = "github:tromshusky/huskyos";
   outputs = { huskyos, ... } : huskyos.grub { 
-    efi-device = ./EFI;
-    btrfs-device = ./BTR;
+    efi-device = builtins.readFile ./EFI;
+    btrfs-device = builtins.readFile ./BTR;
     this-flake = ./flake.nix;
     hardware-configuration-no-filesystems = ./hardware-configuration-no-filesystems.nix;
   };
