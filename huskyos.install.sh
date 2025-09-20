d67c0054af984d7974b7fc916e76eb89fc0678fc
tromshusky <tromshusky@gmail.com>
2025-09-20 11:30:52 +0000

    bugfix 404

--- huskyos.install.sh
+++ huskyos.install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 [ -v DISK ] ||
 DISK=/dev/$(nix-shell -p zenity --command "zenity --list --column 'ASD' --column 'EFE' --column 'ZXZ' --column 'YH' \$(lsblk -o NAME,SIZE,TYPE,TRAN | grep disk)")
-sh <(curl https://raw.githubusercontent.com/tromshusky/huskyos/huskyos-tools/huskyos.install.entire.disk.sh)
+sh <(curl https://raw.githubusercontent.com/tromshusky/huskyos/huskyos-tools/huskyos.install.entire.disk.efi.grub.sh)
