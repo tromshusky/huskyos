40f8b05d6e07f64152c04eb54b8f0b94bac909a0
tromshusky <tromshusky@gmail.com>
2025-09-20 11:56:55 +0000

    bugfix: replaced commanda as environment variables with functions

--- huskyos.install.entire.disk.efi.grub.sh
+++ huskyos.install.entire.disk.efi.grub.sh
@@ -1,38 +1,38 @@
 #!/bin/sh
 
 # assertions
-cond_btrfs="which btrfs >/dev/null"
-cond_curl="which curl >/dev/null"
-cond_diskexs="lsblk $DISK"
-cond_diskpar="[ -v DISK ]"
-cond_mkbtr="which mkfs.btrfs >/dev/null"
-cond_mkfat="which mkfs.fat >/dev/null"
-cond_nixgen="which nixos-generate-config >/dev/null"
-cond_nixinst="which nixos-install >/dev/null"
-cond_parted="which parted >/dev/null"
-cond_root="[ $EUID -eq 0 ]"
+cond_btrfs(){ which btrfs >/dev/null; }
+cond_curl(){ which curl >/dev/null; }
+cond_diskexs(){ lsblk $DISK; }
+cond_diskpar(){ [ -v DISK ]; }
+cond_mkbtr(){ which mkfs.btrfs >/dev/null; }
+cond_mkfat(){ which mkfs.fat >/dev/null; }
+cond_nixgen(){ which nixos-generate-config >/dev/null; }
+cond_nixinst(){ which nixos-install >/dev/null; }
+cond_parted(){ which parted >/dev/null; }
+cond_root(){ [ $EUID -eq 0 ]; }
 
-$cond_btrfs || echo "btrfs: no such command" >&2
-$cond_curl || echo "curl: no such command" >&2
-$cond_diskexs || echo "$DISK is not a disk" >&2
-$cond_diskpar || echo "DISK is not set" >&2
-$cond_mkbtr || echo "mkfs.btrfs: no such command" >&2
-$cond_mkfat || echo "mkfs.fat: no such command" >&2
-$cond_nixgen || echo "nixos-generate-config: no such command" >&2
-$cond_nixinst || echo "nixos-install: no such command" >&2
-$cond_parted || echo "parted: no such command" >&2
-$cond_root || echo "please run as root" >&2
+cond_btrfs || echo "btrfs: no such command" >&2
+cond_curl || echo "curl: no such command" >&2
+cond_diskexs || echo "$DISK is not a disk" >&2
+cond_diskpar || echo "DISK is not set" >&2
+cond_mkbtr || echo "mkfs.btrfs: no such command" >&2
+cond_mkfat || echo "mkfs.fat: no such command" >&2
+cond_nixgen || echo "nixos-generate-config: no such command" >&2
+cond_nixinst || echo "nixos-install: no such command" >&2
+cond_parted || echo "parted: no such command" >&2
+cond_root || echo "please run as root" >&2
 
-$cond_btrfs || exit
-$cond_curl || exit
-$cond_diskexs || exit
-$cond_diskpar || exit
-$cond_mkbtr || exit
-$cond_mkfat || exit
-$cond_nixgen || exit
-$cond_nixinst || exit
-$cond_parted || exit
-$cond_root || exit
+cond_btrfs || exit
+cond_curl || exit
+cond_diskexs || exit
+cond_diskpar || exit
+cond_mkbtr || exit
+cond_mkfat || exit
+cond_nixgen || exit
+cond_nixinst || exit
+cond_parted || exit
+cond_root || exit
 
 # variables
 
