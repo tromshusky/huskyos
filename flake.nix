71127a50ebdec8ca6a9f950b0bbce72725f530f5
tromshusky <tromshusky@gmail.com>
2025-09-21 10:49:00 +0000

    added root pw loading from file

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
   outputs = { huskyos, ... } : huskyos.grub { 
     efi-device = builtins.readFile ./EFI;
     btrfs-device = builtins.readFile ./BTR;
+    hashed-root-password = builtins.readFile ./RPW;
     this-flake = ./flake.nix;
     hardware-configuration-no-filesystems = ./hardware-configuration-no-filesystems.nix;
   };
