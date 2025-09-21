6473adf30ee0ad0895f067cd01a8ec18e042e788
tromshusky <tromshusky@gmail.com>
2025-09-21 20:51:35 +0000

    Update etc-nixos-backup.nix

--- etc-nixos-backup.nix
+++ etc-nixos-backup.nix
@@ -1,4 +1,7 @@
 { config, ... }:{
   environment.etc."nixos/backup/flake.nix".source = config.huskyos.flakeUri;
   environment.etc."nixos/backup/hardware-configuration-no-filesystems.nix".source = config.huskyos.hardwareUri;
+  environment.etc."nixos/backup/EFI".source = config.huskyos.efiDevice;
+  environment.etc."nixos/backup/BTR".source = config.huskyos.btrfsDevice;
+  environment.etc."nixos/backup/RPW".source = config.huskyos.hashedRootPassword;
 }
