bbe25477fd5deadd345f2f7c366c96cbb6927932
tromshusky <tromshusky@gmail.com>
2025-09-21 11:51:54 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   outputs = { huskyos, ... } : huskyos.grub { 
     efi-device = builtins.readFile ./EFI;
     btrfs-device = builtins.readFile ./BTR;
-    hashed-root-password = builtins.readFile ./RPW;
+    hashed-root-password = if (builtins.pathExists ./RPW) && (builtins.readFileType ./RPW == "regular") then builtins.readFile ./RPW else null;
     this-flake = ./flake.nix;
     hardware-configuration-no-filesystems = ./hardware-configuration-no-filesystems.nix;
   };
