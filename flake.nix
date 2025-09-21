d158136ff143a5fe47121cbcea7a68ff343d85f8
tromshusky <tromshusky@gmail.com>
2025-09-21 23:45:31 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,6 +6,6 @@
     hashed-root-password = ./RPW;
     this-flake = ./flake.nix;
     hardware-configuration-no-filesystems = ./hardware-configuration-no-filesystems.nix;
-    nix-extra-config = ./config.nix;
+    nixos-extra-config = {};
   };
 }
