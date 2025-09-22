65d6552c80cb3c4691af1a074c33c71ed92a1dc6
tromshusky <tromshusky@gmail.com>
2025-09-22 23:40:33 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     efi-device = ./EFI;
     btrfs-device = ./BTR;
     hashed-root-password = ./RPW;
-    this-flake = ./flake.nix;
+    this-flake = "${./.}";
     hardware-configuration-no-filesystems = ./hardware-configuration-no-filesystems.nix;
     nixos-extra-config = {};
   };
