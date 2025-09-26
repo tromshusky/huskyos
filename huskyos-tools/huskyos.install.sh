3968d793522c5184bca21c1a114627ae0123a0be
tromshusky <tromshusky@gmail.com>
2025-09-26 18:32:44 +0200

    esc/dont set variable

--- huskyos-tools/huskyos.install.sh
+++ huskyos-tools/huskyos.install.sh
@@ -15,59 +15,62 @@ if ! [ -v HUSKYOS_ROOT_PW ]; then
   };
 fi;
 
-[ -v HUSKYOS_KBD_LAYOUT ] || export HUSKYOS_KBD_LAYOUT=${
-  zenity --list \
-  --title="Select your keyboard layout" \
-  --text="" \
-  --hide-column=1 \
-  --column="" \
-  --column="Language Name" \
-  "us" "English (US)" \
-  "ar" "Arabic" \
-  "am" "Amharic" \
-  "bn" "Bengali" \
-  "br" "Brazilian Portuguese" \
-  "bg" "Bulgarian" \
-  "ca" "Canadian" \
-  "ch" "Swiss German" \
-  "cz" "Czech" \
-  "de" "German" \
-  "dk" "Danish" \
-  "es" "Spanish" \
-  "et" "Estonian" \
-  "fi" "Finnish" \
-  "fr" "French" \
-  "gr" "Greek" \
-  "he" "Hebrew" \
-  "hr" "Croatian" \
-  "hu" "Hungarian" \
-  "is" "Icelandic" \
-  "it" "Italian" \
-  "ja" "Japanese" \
-  "jp" "Japanese" \
-  "ko" "Korean" \
-  "lt" "Lithuanian" \
-  "lv" "Latvian" \
-  "mk" "Macedonian" \
-  "ml" "Malayalam" \
-  "mt" "Maltese" \
-  "ne" "Nepali" \
-  "no" "Norwegian" \
-  "pl" "Polish" \
-  "ro" "Romanian" \
-  "ru" "Russian" \
-  "si" "Sinhalese" \
-  "sk" "Slovak" \
-  "sw" "Swahili" \
-  "ta" "Tamil" \
-  "te" "Telugu" \
-  "th" "Thai" \
-  "tl" "Tetum" \
-  "tr" "Turkish" \
-  "vi" "Vietnamese" \
-  "xh" "Xhosa" \
-  "yo" "Yoruba" \
-  "zu" "Zulu"
+[ -v HUSKYOS_KBD_LAYOUT ] || {
+  choice=${
+    zenity --list \
+    --title="Select your keyboard layout" \
+    --text="" \
+    --hide-column=1 \
+    --column="" \
+    --column="Language Name" \
+    "us" "English (US)" \
+    "ar" "Arabic" \
+    "am" "Amharic" \
+    "bn" "Bengali" \
+    "br" "Brazilian Portuguese" \
+    "bg" "Bulgarian" \
+    "ca" "Canadian" \
+    "ch" "Swiss German" \
+    "cz" "Czech" \
+    "de" "German" \
+    "dk" "Danish" \
+    "es" "Spanish" \
+    "et" "Estonian" \
+    "fi" "Finnish" \
+    "fr" "French" \
+    "gr" "Greek" \
+    "he" "Hebrew" \
+    "hr" "Croatian" \
+    "hu" "Hungarian" \
+    "is" "Icelandic" \
+    "it" "Italian" \
+    "ja" "Japanese" \
+    "jp" "Japanese" \
+    "ko" "Korean" \
+    "lt" "Lithuanian" \
+    "lv" "Latvian" \
+    "mk" "Macedonian" \
+    "ml" "Malayalam" \
+    "mt" "Maltese" \
+    "ne" "Nepali" \
+    "no" "Norwegian" \
+    "pl" "Polish" \
+    "ro" "Romanian" \
+    "ru" "Russian" \
+    "si" "Sinhalese" \
+    "sk" "Slovak" \
+    "sw" "Swahili" \
+    "ta" "Tamil" \
+    "te" "Telugu" \
+    "th" "Thai" \
+    "tl" "Tetum" \
+    "tr" "Turkish" \
+    "vi" "Vietnamese" \
+    "xh" "Xhosa" \
+    "yo" "Yoruba" \
+    "zu" "Zulu";
+  } &&
+  export HUSKYOS_KBD_LAYOUT=$choice;
 };
 
 
