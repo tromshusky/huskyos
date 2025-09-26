a614b99f217ff7b8ad71f0d27935911dc4d440b2
tromshusky <tromshusky@gmail.com>
2025-09-26 14:14:25 +0200

    changed huskyos-tools github path to local

--- huskyos-tools.nix
+++ huskyos-tools.nix
@@ -1,12 +1,6 @@
 { pkgs, ... }:
 let
-    huskyos-tools-source = pkgs.fetchFromGitHub {
-        owner = "tromshusky";
-        repo = "huskyos";
-        rev = "huskyos-tools";
-        hash = "sha256-nBQAtzu6Q3IZJV7rgyWqJQAx0Fr+Nz1ayt2EbvO2QJ4=";
-    };
-    huskyos-tools = (import "${huskyos-tools-source}/package.nix" pkgs);
+    huskyos-tools = (import ./huskyos-tools/package.nix pkgs);
 in {
     environment.systemPackages = [ huskyos-tools ];
 }
