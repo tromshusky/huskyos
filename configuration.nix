96ea76bce17d596c2429edb0bccc28b17e766e60
tromshusky <tromshusky@gmail.com>
2025-09-27 19:57:51 +0200

    refactor home .var module

--- configuration.nix
+++ configuration.nix
@@ -11,13 +11,13 @@
       ./flathub.nix
       ./gnome.nix
       ./grub.nix
+      ./home-var-bindmount.nix
       ./huskyos-tools.nix
       ./huskyos-options.nix
       ./keyboard-layout.nix
       ./user.nix
       ./plymouth.nix
       ./root-pw.nix
-      ./split-home.nix
       ./steam.nix
     ];
 }
