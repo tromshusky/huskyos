62f93a55e3939d41f0f6975d296ad0e8e50abf16
tromshusky <tromshusky@gmail.com>
2025-10-10 14:18:07 +0200

    bugfix: zenity not showing durin update

--- huskyos-tools/huskyos.update.sh
+++ huskyos-tools/huskyos.update.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-systemctl start nixos-upgrade.service | nix-shell -p zenity --run "zenity --progress --pulsate--text='Update'"
+systemctl start nixos-upgrade.service | nix-shell -p zenity --run "zenity --progress --pulsate --text='Update'"
