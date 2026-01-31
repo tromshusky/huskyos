2ba8ce618a10cfdc0909a62f72743a4dd76a5db6
tromshusky <tromshusky@gmail.com>
2026-01-31 17:10:34 +0000

    Update flake.nix
    issue #32 #33

--- flake.nix
+++ flake.nix
@@ -1,11 +1,14 @@
 {
+#  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+#  inputs.huskyos.inputs.nixpkgs.follows = "nixpkgs";
   inputs.huskyos.url = "github:tromshusky/huskyos";
   outputs = { huskyos, ... } : huskyos.grub { 
     efi-device = ./EFI;
     btrfs-device = ./BTR;
     hashed-root-password = ./RPW;
+    keyboard-layout = ./KBD;
     this-flake = "${./.}";
     hardware-configuration-no-filesystems = ./hardware-configuration-no-filesystems.nix;
-    nixos-extra-config = {};
+#    nixos-extra-config = ./config.nix;
   };
 }
