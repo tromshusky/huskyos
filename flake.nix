be535259200e71d706c6469f96b317fde8bcf481
tromshusky <tromshusky@gmail.com>
2026-02-03 10:27:04 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,45 +1,46 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs = { nixpkgs, ... }: { os = 
-    myFlake:
-    let
-
-      base = "${myFlake.outPath}/BASE";
-      keyboard-layout = "${myFlake.outPath}/KBD";
-      hashed-root-password = "${myFlake.outPath}/RPW";
-      btrfs-device = "${myFlake.outPath}/BTR";
-      efi-device = "${myFlake.outPath}/EFI";
-      hardware-configuration-no-filesystems = "${myFlake.outPath}/hardware-configuration-no-filesystems.nix";
-      extra-config = fileThatExistsMapElse "${myFlake.outPath}/config.nix" (_: _) { };
-
-      fileThatExistsMapElse =
-        fPath: mapFile: els:
-        if (builtins.pathExists fPath) && (builtins.readFileType fPath == "regular") then
-          (mapFile fPath)
-        else
-          els;
-
-      firstLineOfFileElse = fPath: els: (fileThatExistsMapElse fPath firstLine els);
-
-      firstLine = text: (builtins.head (builtins.split "\n" (builtins.readFile text)));
-
-    in
+  outputs =
+    { nixpkgs, ... }:
     {
-      nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
-        modules = [
-          hardware-configuration-no-filesystems
-          ./configuration.nix
-          {
-            huskyos.btrfsDevice = builtins.readFile btrfs-device;
-            huskyos.efiDevice = builtins.readFile efi-device;
-            huskyos.flakeFolder = myFlake.outPath;
-            huskyos.hardwareUri = hardware-configuration-no-filesystems;
-            huskyos.keyboardLayout = firstLineOfFileElse keyboard-layout "us";
-            huskyos.hashedRootPassword = firstLineOfFileElse hashed-root-password null;
-          }
-          extra-config
-        ];
-      };
+      withSelf =
+        { outPath, ... }:
+        let
+          base = "${outPath}/BASE";
+          keyboard-layout = "${outPath}/KBD";
+          hashed-root-password = "${outPath}/RPW";
+          btrfs-device = "${outPath}/BTR";
+          efi-device = "${outPath}/EFI";
+          hardware-configuration-no-filesystems = "${outPath}/hardware-configuration-no-filesystems.nix";
+          extra-config = fileThatExistsMapElse "${outPath}/config.nix" (_: _) { };
+
+          fileThatExistsMapElse =
+            fPath: mapFile: els:
+            if (builtins.pathExists fPath) && (builtins.readFileType fPath == "regular") then
+              (mapFile fPath)
+            else
+              els;
+          firstLineOfFileElse = fPath: els: (fileThatExistsMapElse fPath firstLine els);
+          firstLine = text: (builtins.head (builtins.split "\n" (builtins.readFile text)));
+
+          buildArg = {
+            modules = [
+              hardware-configuration-no-filesystems
+              ./configuration.nix
+              {
+                huskyos.btrfsDevice = builtins.readFile btrfs-device;
+                huskyos.efiDevice = builtins.readFile efi-device;
+                huskyos.flakeFolder = outPath;
+                huskyos.hardwareUri = hardware-configuration-no-filesystems;
+                huskyos.keyboardLayout = firstLineOfFileElse keyboard-layout "us";
+                huskyos.hashedRootPassword = firstLineOfFileElse hashed-root-password null;
+              }
+              extra-config
+            ];
+          };
+        in
+        {
+          nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem buildArg;
+        };
     };
-  };
 }
