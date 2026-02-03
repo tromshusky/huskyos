69c1d8c095078427e98024b0bd8328e38f5c2e69
tromshusky <tromshusky@gmail.com>
2026-02-03 11:55:24 +0000

    apply new api to template flake

--- template/flake.nix
+++ template/flake.nix
@@ -1,10 +1,10 @@
 {
-## the stable nixos branch gets published for the month 05 and 11 every year.
+## the stable nixos branch gets published in the month 05 and 11 every year.
 ## the rolling release branch is selected by default "github:NixOS/nixpkgs/nixos-unstable";
 
 #  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.huskyos.inputs.nixpkgs.follows = "nixpkgs";
   inputs.huskyos.url = "github:tromshusky/huskyos";
-  outputs = { huskyos, self, ... } : huskyos.${builtins.replaceStrings ["\n"] [""] (builtins.readFile ./EDITION)} self;
+  outputs = { huskyos, self, ... } : huskyos.withSelf self;
 }
