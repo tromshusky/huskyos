11a28a34b4ecba5f564b1bd33941f88fb747aa8d
tromshusky <tromshusky@gmail.com>
2026-03-02 23:22:39 +0000

    somehow this works always

--- update-gui.nix
+++ update-gui.nix
@@ -8,7 +8,7 @@ let
   mainServiceName = "nixos-upgrade.service";
   acName = "huskyos-activate";
   guiName = "huskyos-upgrade";
-  LOGFILENAME = "/huskyupdate.txt";
+  LOGFILENAME = "/myupdatelog.txt";
 
   activationService.enable = true;
   activationService.description = "Activate newest system";
