c984a415f9b94b92d99050f423254c5f7adba446
tromshusky <tromshusky@gmail.com>
2026-03-02 23:14:41 +0000

    square 1

--- update-gui.nix
+++ update-gui.nix
@@ -8,7 +8,7 @@ let
   mainServiceName = "nixos-upgrade.service";
   acName = "huskyos-activate";
   guiName = "huskyos-upgrade";
-  LOGFILENAME = "/SystemUpdate.txt";
+  LOGFILENAME = "/myupdatelog.txt";
 
   activationService.enable = true;
   activationService.description = "Activate newest system";
