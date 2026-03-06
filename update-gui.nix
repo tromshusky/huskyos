95f5638a92f2715e749055dce594694ef789cd9f
tromshusky <tromshusky@gmail.com>
2026-03-06 19:01:57 +0000

    made first update notification urgency low

--- update-gui.nix
+++ update-gui.nix
@@ -17,7 +17,7 @@ let
 
   guiService.description = "GUI wrapper for system upgrade";
   guiService.enable = true;
-  guiService.serviceConfig.ExecStart = "${upgradeNotifyScript}";
+  guiService.serviceConfig.ExecStart = lib.mkForce "${upgradeNotifyScript}";
   guiService.serviceConfig.Type = "simple";
   guiService.serviceConfig.KillMode= "control-group";
   guiService.unitConfig.PartOf = [ mainServiceName ];
@@ -26,16 +26,12 @@ let
   guiService.unitConfig.ConditionPathExistsGlob = "/run/user/*";
   guiService.wantedBy = [ mainServiceName ];
 
-  guiUserService.description = "";
-  guiUserService.serviceConfig.Type = "simple";
-  guiUserService.serviceConfig.ExecStart = "${upgradeNotifyUserScript}";
-
   upgradeNotifyUserScript = pkgs.writeShellScript "myscript" ''
         export PATH=$PATH:/run/current-system/sw/bin:${pkgs.libnotify}/bin
         export XDG_RUNTIME_DIR="/run/user/$(id --user)"
         gnAni=/org/gnome/desktop/interface/enable-animations
 
-        ntfBase() { notify-send --urgency critical --app-icon "folder-download-symbolic" --app-name "System Update" "$@"; }
+        ntfBase() { notify-send --urgency low --app-icon "folder-download-symbolic" --app-name "System Update" "$@"; }
         ntfQuiet() { ntfBase --replace-id "$ID" "$@"; }
         ntf() { ID=$(ntfQuiet --print-id "$@"); }
         ntfExit() {
@@ -49,6 +45,14 @@ let
             "$ID";
           exit "$2";
         }
+        removeNtf() {
+          gdbus call \
+            --session \
+            --dest org.freedesktop.Notifications \
+            --object-path /org/freedesktop/Notifications \
+            --method org.freedesktop.Notifications.CloseNotification \
+            "$ID";
+        }
         ID=$(ntfBase --print-id "Updating the system...")
         activate() {
           systemctl start ${acName}.service && (
@@ -61,6 +65,9 @@ let
           [ "$( realpath /nix/var/nix/profiles/system )" == "$( realpath /run/current-system )" ] && ntfExit "System is already up to date." 0 ;
           [ "$(dconf read $gnAni-backup)" == "" ] && dconf write $gnAni-backup $(dconf read $gnAni);
           dconf write $gnAni false;
+          removeNtf;
+          sleep 1;
+          ntf --urgency critical "Update Completed...";
           answ=$(ntfQuiet --action n=No --action y=Activate "Update completed. Activate immediately?")
           dconf write $gnAni $(dconf read $gnAni-backup);
           case "$answ" in
