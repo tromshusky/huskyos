a7c5cbcc21d4b14cc9da48e7b198f2435489b2d6
tromshusky <tromshusky@gmail.com>
2026-04-11 23:52:51 +0000

    First iteration of uki boot, a bootloader alternative
    added uki generation and installation as default disk bootloader.

---
+++ ukiboot.nix
@@ -0,0 +1,42 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  initCmdline = [ "init=/nix/var/nix/profiles/system/init" ];
+  ukiConfig.imports = [ ./hardware-configuration-no-filesystems.nix ];
+  ukiConfig.fileSystems = config.fileSystems;
+  ukiConfig.boot.plymouth = config.boot.plymouth;
+  ukiConfig.boot.kernelParams = config.boot.kernelParams ++ initCmdline;
+  ukiConfig.boot.kernelPackages = pkgs.linuxPackages; # the normal kernel is lts (3 years support), latest is 3 month support, testing 0
+  ukiSystemArgs.modules = [ ukiConfig ];
+  ukiDir = config.specialisation.ukiboot.configuration.system.build.uki;
+
+  # ukidir=$( nix build ${./possibleSeperateFlake/}#nixosConfigurations.nixos.config.specialisation.ukiboot.configuration.system.build.uki --no-link --print-out-paths) || exit 1;
+  ukiInstallHook = pkgs.writeShellScript "uki-install-hook" ''
+    PATH=$PATH:/run/current-system/sw/bin;
+    echo building uki kernel...;
+    #ukidir=... # possible lazy build process
+    uki=${ukiDir}/nixos.efi;
+    echo atomic swapping existing kernel...;
+    tmpdir=$(mktemp -d) &&
+    mount ${builtins.readFile ./EFI} $tmpdir &&
+    mkdir -p $tmpdir/efi/boot;
+    cd $tmpdir/efi/boot &&
+    cp $uki BOOTX64-next.EFI &&
+    sync &&
+    mv BOOTX64-next.EFI BOOTX64.EFI || exit 1;
+    echo ...uki bootloader sucessfully installed.;
+    cd / &&
+    umount $tmpdir;
+  '';
+in
+{
+  specialisation.ukiboot.inheritParentConfig = false;
+  specialisation.ukiboot.configuration = ukiConfig;
+
+  boot.loader.external.enable = true;
+  boot.loader.external.installHook = ukiInstallHook;
+}
